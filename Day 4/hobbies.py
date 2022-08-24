6e03f0f67c431bc033b7a0671ac2c8b0a7bb4fc0
Arthur Nepal <111441367+iamarthurphobia@users.noreply.github.com>
2022-08-24 22:58:26 +0545

    Add files via upload

---
+++ Day 4/hobbies.py
@@ -0,0 +1,9 @@
+# using for loop to print my hobbies
+
+print("My Hobbies:")
+
+hobbie = ['padne','khane','hagne','sutne']
+
+for item in hobbie:
+  
+  print(item)
---
+++ Day 4/sequence of number.py
@@ -0,0 +1,8 @@
+# sequence of numbers from 1 to 10
+num = range(1,11)
+
+sum = 0
+
+for item in num:
+   sum += item
+print("Sum =",sum)
